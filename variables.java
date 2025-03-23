e3ede75a49b205126ae79492e0238d8ddced1a95
AzorAha1 <wizfaiz@icloud.com>
2025-03-23 13:48:08 +0100

    understanding variables in java

---
+++ variables.java
@@ -0,0 +1,16 @@
+public class variables {
+    int year = 2025;
+    double gpa = 3.6;
+    boolean isMale = true;
+    char grade = 'A';
+    String name = "faisal";
+    public static void main(String[] args) {
+        variables myobj = new variables();
+        System.out.println(myobj.year);
+        System.out.println(myobj.gpa);
+        System.out.println(myobj.isMale);
+        System.out.println(myobj.grade);
+        System.out.println(myobj.name);
+
+    }
+}
