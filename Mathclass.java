14350e8d376977045a074fbc1b52279863c355b4
AzorAha1 <wizfaiz@icloud.com>
2025-03-24 14:59:00 +0100

    calculating hyptenuse, circumference of a circle, area of a circl and volume of a sphere

--- Mathclass.java
+++ Mathclass.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Mathclass {
     public static void main(String[] args) {
         System.out.println(Math.abs(-4.7));
@@ -13,5 +14,27 @@ public class Mathclass {
         System.out.println(Math.hypot(3, 4));
         System.out.println(Math.PI);
         System.out.println(Math.E);
+
+        //calculate the hypotenuse of a right-angled triangle
+        Scanner myScanner = new Scanner(System.in);
+        double side1;
+        double side2;
+        System.out.println("Enter the value of side1: ");
+        side1 = myScanner.nextDouble();
+        System.out.println("Enter the value of side2: ");
+        side2 = myScanner.nextDouble();
+        System.out.println("The hypotenuse of the right-angled triangle is: " + Math.sqrt(Math.pow(side1, 2) + Math.pow(side2, 2)));
+
+        //calculate the circumference of a circle
+        double radius;
+        System.out.println("Enter the value of the radius: ");
+        radius = myScanner.nextDouble();
+        System.out.println("The circumference of the circle is: " + (2 * Math.PI * radius));
+
+        //calculate the area of a circle
+        System.out.println("The area of the circle is: " + (Math.PI * Math.pow(radius, 2)));
+
+        //volume of a sphere
+        System.out.println("The volume of the sphere is: " + ((4/3) * Math.PI * Math.pow(radius, 3)) + " cm^3");
     }
 }
