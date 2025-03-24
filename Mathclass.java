ed45fc30be520730822d9018b8d9e190483f5c37
AzorAha1 <wizfaiz@icloud.com>
2025-03-24 14:50:17 +0100

    math class

---
+++ Mathclass.java
@@ -0,0 +1,17 @@
+public class Mathclass {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-4.7));
+        System.out.println(Math.ceil(7.4));
+        System.out.println(Math.floor(7.8));
+        System.out.println(Math.max(8.6, 5.2));
+        System.out.println(Math.min(8.6, 5.2));
+        System.out.println(Math.pow(5, 3));
+        System.out.println(Math.sqrt(64));
+
+        
+        System.out.println(Math.cbrt(64));
+        System.out.println(Math.hypot(3, 4));
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+    }
+}
