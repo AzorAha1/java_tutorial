e4ac6855dd968193c6bbb7f8bedf73c075de94fa
AzorAha1 <wizfaiz@icloud.com>
2025-04-21 10:39:39 +0100

    creating classes and objects created from that class

---
+++ Houses.java
@@ -0,0 +1,11 @@
+public class Houses {
+    public String houseName;
+    String motto;
+    String region;
+    String Sigil;
+    String ruler;
+
+    public void printout() {
+        System.out.printf("House Name is %s, the motto is %s, region is %s and Sigil is a %s and current ruler is %s", houseName, motto,region, Sigil, ruler);
+    }
+}
---
+++ callingclasses.java
@@ -0,0 +1,12 @@
+
+public class callingclasses {
+    public static void main(String[] args) {
+        Houses stark = new Houses();
+        stark.houseName = "Stark";
+        stark.Sigil = "Direwolf";
+        stark.motto = "Winter is Coming";
+        stark.region = "North";
+        stark.ruler = "Sansa Stark";
+        stark.printout();
+    }
+}
