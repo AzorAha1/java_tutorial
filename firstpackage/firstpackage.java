142342861371c2441127635cb8a9c6560a7c691c
AzorAha1 <wizfaiz@icloud.com>
2025-05-01 18:55:34 +0100

    understanding using packages to organize code better

---
+++ firstpackage/firstpackage.java
@@ -0,0 +1,9 @@
+package firstpackage;
+
+public class firstpackage {
+    public static void main(String[] args) {
+    }
+    public void firstMethod() {
+        System.out.println("This is the first method");
+    }
+}
---
+++ secondpackage/secondpackage.java
@@ -0,0 +1,9 @@
+package secondpackage;
+import firstpackage.firstpackage;
+public class secondpackage {
+    public static void main(String[] args) {
+        firstpackage first = new firstpackage();
+        first.firstMethod();
+    }
+    
+}
